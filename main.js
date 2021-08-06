ab4c854ed14b07cec1ed546ec11da5fb922d8d8f
Rawanzaqout <zrawan132@gmail.com>
2021-08-07 01:52:59 +0300

    create index.js file relates #33

---
+++ main.js
@@ -0,0 +1,9 @@
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-mode");
+    if ( document.body.classList.contains("dark-mode")) {
+        icon.src="../assets/sun.svg";
+    }else{
+        icon.src="../assets/dark-mode.png";
+    }
+}
