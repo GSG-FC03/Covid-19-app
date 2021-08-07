460d35125b4b3fdc8e753f866dae47cecc82c771
Rawanzaqout <zrawan132@gmail.com>
2021-08-07 22:55:19 +0300

    change path

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var icon = document.getElementById("icon");
 icon.onclick = function(){
     document.body.classList.toggle("dark-mode");
     if ( document.body.classList.contains("dark-mode")) {
-        icon.src="../assets/sun.svg";
+        icon.src="./assets/sun.svg";
     }else{
-        icon.src="../assets/dark-mode.png";
+        icon.src="./assets/dark-mode.png";
     }
 }
