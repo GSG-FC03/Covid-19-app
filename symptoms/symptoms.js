c44625680f3400fb099059fa04f4586e15298533
NadaSaleh20 <nadasaleh987@gmail.com>
2021-08-08 17:10:38 +0300

    Add function fot dark and light mode in js file Relates #31

---
+++ symptoms/symptoms.js
@@ -0,0 +1,11 @@
+//dark mode 
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-mode");
+    if ( document.body.classList.contains("dark-mode")) {
+        icon.src="../assets/sun.svg";
+
+    }else{
+        icon.src="../assets/dark-mode.png";
+    }
+}
