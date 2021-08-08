ad21d4dc2fd10a064cf2cb22d0c83dd07229c0ee
Rawanzaqout <zrawan132@gmail.com>
2021-08-08 13:14:00 +0300

    Add an event listener to the Start button to go to the home page relates #33

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 var icon = document.getElementById("icon");
-icon.onclick = function(){
+const button = document.querySelector("#startButton");
+
+
+button.addEventListener("click", logIn);
+function logIn() {
+  if (startButton.value !== "start") {
+    window.location.href = './home/home.html';
+}
+}
+
+
+    icon.onclick = function(){
     document.body.classList.toggle("dark-mode");
     if ( document.body.classList.contains("dark-mode")) {
         icon.src="./assets/sun.svg";
