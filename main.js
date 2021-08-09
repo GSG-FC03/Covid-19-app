3310fcb30c69523a4d89037b04e9740a282fcc2a
Rawanzaqout <zrawan132@gmail.com>
2021-08-09 23:35:14 +0300

    use const instead of var,delete if statment relates #33

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-var icon = document.getElementById("icon");
+const icon = document.getElementById("icon");
 const button = document.querySelector("#startButton");
-
 
 button.addEventListener("click", logIn);
 function logIn() {
-  if (startButton.value !== "start") {
-    window.location.href = './home/home.html';
-}
+  window.location.href = "./home/home.html";
 }
 
-
-    icon.onclick = function(){
-    document.body.classList.toggle("dark-mode");
-    if ( document.body.classList.contains("dark-mode")) {
-        icon.src="./assets/sun.svg";
-    }else{
-        icon.src="./assets/dark-mode.png";
-    }
-}
+icon.onclick = function () {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    icon.src = "./assets/sun.svg";
+  } else {
+    icon.src = "./assets/dark-mode.png";
+  }
+};
