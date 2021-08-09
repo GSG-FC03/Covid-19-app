ec15940f0da34119b4c99178af5c8fd123d4c9d8
montherismail <montherismail90@gmail.com>
2021-08-09 12:33:28 +0300

    change var to const

--- advice/advice.js
+++ advice/advice.js
@@ -1,19 +1,19 @@
-    /*************** Back Icon *********************/
-         function backIcon(){
-             window.location.href ="../symptoms/Symptoms.html";
-         }
+/*************** Back Icon *********************/
+function backIcon() {
+    window.location.href = "../symptoms/Symptoms.html";
+}
 
-    /***************Dark Mode***********************/
-        var icon = document.getElementById("icon");
-        icon.onclick = function(){
-            document.body.classList.toggle("dark-mode");
-            if ( document.body.classList.contains("dark-mode")) {
-                icon.src="../assets/sun.svg";
-            }else{
-                icon.src="../assets/dark-mode.png";
-            }
-        }
-    /***************** Next Button ******************/
-        function reDirect(){
-            window.location.href ="../contact/contact.html";
-        }
+/***************Dark Mode***********************/
+const icon = document.getElementById("icon");
+icon.onclick = function () {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        icon.src = "../assets/sun.svg";
+    } else {
+        icon.src = "../assets/dark-mode.png";
+    }
+}
+/***************** Next Button ******************/
+function reDirect() {
+    window.location.href = "../contact/contact.html";
+}
