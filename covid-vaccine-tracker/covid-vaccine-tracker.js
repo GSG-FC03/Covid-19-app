e0839473ae61a5061a9f6fa8904c7e915eaf0abb
Rawanzaqout <zrawan132@gmail.com>
2021-08-12 18:12:55 +0300

    remove  console log in js page

--- covid-vaccine-tracker/covid-vaccine-tracker.js
+++ covid-vaccine-tracker/covid-vaccine-tracker.js
@@ -1,19 +1,19 @@
-const  icon = document.getElementById("icon");
-icon.onclick = function(){
-    document.body.classList.toggle("dark-mode");
-    if ( document.body.classList.contains("dark-mode")) {
-        icon.src="../assets/sun.svg";
-    }else{
-        icon.src="../assets/dark-mode.png";
-    }
-}
+const icon = document.getElementById("icon");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    icon.src = "../assets/sun.svg";
+  } else {
+    icon.src = "../assets/dark-mode.png";
+  }
+};
 
 document.getElementById("back").onclick = function () {
   location.href = "../home/home.html";
-}
+};
 //popup
 const button = document.querySelector("#Symptoms-button");
-const myCircule = document.getElementById("myCircule")
+const myCircule = document.getElementById("myCircule");
 button.addEventListener("click", move);
 function move() {
   window.location.href = "../Symptoms/Symptoms.html";
@@ -26,9 +26,12 @@ openModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const modal = document.querySelector(button.dataset.modalTarget);
     openModal(modal);
-    myCircule.style.setProperty('stroke-dasharray',( (156-156*totalPerHundred).toString()+" "+(156*totalPerHundred).toString()))
-    console.log(totalPerHundred);
-   
+    myCircule.style.setProperty(
+      "stroke-dasharray",
+      (156 - 156 * totalPerHundred).toString() +
+        " " +
+        (156 * totalPerHundred).toString()
+    );
   });
 });
 
@@ -280,7 +283,7 @@ function createDatalist() {
     createNewOption();
   }
 }
-let totalPerHundred ;
+let totalPerHundred;
 function getVaccineData(countrycode) {
   const url = `https://disease.sh/v3/covid-19/vaccine/coverage/countries/${countrycode}?lastdays=30&fullData=true`;
   let i;
@@ -290,9 +293,9 @@ function getVaccineData(countrycode) {
       for (i = data.timeline.length - 1; i >= 0; i--) {
         if (data.timeline[i].daily > 0) break;
       }
-     
+
       total.textContent = data.timeline[i].total;
       number.appendChild(total);
-      totalPerHundred=(data.timeline[i].totalPerHundred)/100;
+      totalPerHundred = data.timeline[i].totalPerHundred / 100;
     });
 }
